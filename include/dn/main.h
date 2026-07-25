e3adb1319a1b4a8aa22e30efea4f4e777b461be6
Piotr Doan <piotr.doan@proton.me>
2026-07-25 17:24:24 +0200

    Minor comment fix

--- include/dn/main.h
+++ include/dn/main.h
@@ -1,6 +1,6 @@
 #pragma once
 
-// == EXIT CODES =========================================================== //
+// == EXIT CODES ============================================================ //
 
 // List of known exit codes returned by the library.
 typedef enum DnExitCode : int {
@@ -11,7 +11,7 @@ typedef enum DnExitCode : int {
   DnExitCode_TestFailure = 4,
 } DnExitCode;
 
-// == MAIN ENTRY =========================================================== //
+// == MAIN ENTRY ============================================================ //
 
 // Helps define the main entry point for the library. This is for convenience
 // and for aid in correctness of initialization and deinitialization.
@@ -22,7 +22,7 @@ typedef enum DnExitCode : int {
   } \
   DnExitCode DnMain()
 
-// == LIBRARY INITITIALIZATION ============================================= //
+// == LIBRARY INITITIALIZATION ============================================== //
 
 // Initializes the library. Should be called before any other library functions
 // at the very beginning of main().
